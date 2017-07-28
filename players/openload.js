e3bb1ea4da1ccea443dfcc17e69805446fdd5b55
haleyngonadi <misshaleyn@gmail.com>
2017-07-28 00:07:31 -0400

    idk idk idk

---
+++ players/openload.js
@@ -0,0 +1,13 @@
+chrome.storage.sync.get("active", function(data) {
+  if(typeof data.active == 'undefined' || data.active=='on'){
+      //removeElementsByClass('centeres')
+      
+
+
+      var el = document.querySelector("video");
+
+ console.log(el);
+
+}
+});
+
--- players/univision.js
+++ players/univision.js
@@ -3,9 +3,36 @@ chrome.storage.sync.get("active", function(data) {
       //removeElementsByClass('centeres')
       
 
-      console.log(document.body);
+      document.addEventListener("DOMContentLoaded", function(){
+
+      	
+var video = document.getElementsByTagName("video")[0];
+
+// Wait for video to end
+video.addEventListener('ended', function(e) {
+	// Notify event page
+	chrome.runtime.sendMessage("video_ended", function(response) { });
+});
+
+// Wait for 
+chrome.runtime.onMessage.addListener(function(message, sender, response) {
+	// Check if there is a video player
+	if (video) {
+		// Start video on new tab
+		video.play();
+      }
+}); 
+
+
+   }, false);
+
+
+
+
+
+
+
 
 }
 });
-
 
